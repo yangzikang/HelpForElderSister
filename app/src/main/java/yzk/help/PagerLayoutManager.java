f09cbf66c79b5e9c05417540aa08c1002957603e
yangzikang769 <yangzikang769@pingan.com.cn>
2021-06-04 11:19:04 +0800

    老姐牛逼

--- app/src/main/java/yzk/help/PagerLayoutManager.java
+++ app/src/main/java/yzk/help/PagerLayoutManager.java
@@ -21,4 +21,14 @@ public class PagerLayoutManager extends LinearLayoutManager {
         mSnapHelper.attachToRecyclerView(view);
     }
 
+    /**
+     * https://blog.csdn.net/smileiam/article/details/88396546
+     * 老姐牛逼
+     * @param state
+     * @return
+     */
+    @Override
+    public int getExtraLayoutSpace(RecyclerView.State state) {
+        return 400;
+    }
 }
